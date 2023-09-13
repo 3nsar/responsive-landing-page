4abda6be69294c8d03cdf5c57dc7442d84aa7a34
3nsar <84131267+3nsar@users.noreply.github.com>
2023-09-13 18:44:22 +0200

    commit

--- responsive-landing-page/src/components/Hero.jsx
+++ responsive-landing-page/src/components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
       
       <motion.div className="hero-text" 
         
-        initial={{opacity: 0, scale: 0}}
-        whileInView={{opacity: 1, scale: 1}}
+        initial={{opacity: 0, scale: 0, x: 400}}
+        whileInView={{opacity: 1, scale: 1, x:0}}
         transition={{duration: 0.6}}
         viewport={{once: true}}
         >
