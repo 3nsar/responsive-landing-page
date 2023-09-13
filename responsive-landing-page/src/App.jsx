d5b6193cc74cb77baa91eb03fc871f07695b9216
3nsar <84131267+3nsar@users.noreply.github.com>
2023-09-13 19:19:06 +0200

    added card

--- responsive-landing-page/src/App.jsx
+++ responsive-landing-page/src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import "./App.css"
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Cards from './components/Cards'
 
 const App = () => {
   return (
     <div className='App'>
       <Navbar/>
       <Hero/>
+      <Cards/>
     </div>
   )
 }
--- responsive-landing-page/src/components/Cards.jsx
+++ responsive-landing-page/src/components/Cards.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import robot from '../assets/robot1.jpg'
 
 const Cards = () => {
   return (
-    <div className='cards-container'>
-
+    <div className='card-container'>
+      <div className='card'>
+        <img src={robot} alt="Image Alt Text" />
+        <p>This is the content of the card.</p>
+      </div>
     </div>
   )
 }
