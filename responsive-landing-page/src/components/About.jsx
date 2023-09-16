5ede88592dd7e158baa1c4874cf3066f8d782f28
3nsar <84131267+3nsar@users.noreply.github.com>
2023-09-16 23:38:52 +0200

    commit

--- responsive-landing-page/src/components/About.jsx
+++ responsive-landing-page/src/components/About.jsx
@@ -17,7 +17,18 @@ const About = () => {
        </motion.div>
         <div className="about-content">
           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt vero beatae delectus culpa. Molestiae aliquam velit quasi modi ut consequatur vel, asperiores possimus consectetur delectus tempora repellendus temporibus distinctio facere.</p>
-          <img src={robot} alt="pic" height={300}/>
+          
+        <motion.div 
+        
+        initial={{opacity: 0, scale: 0, x: 200}}
+        whileInView={{opacity: 1, scale: 1, x:0}}
+        transition={{duration: 0.6}}
+        viewport={{once: true}}
+        >
+            <img src={robot} alt="pic" height={300}/> 
+        
+        </motion.div>
+          
         </div>
     </div>
   )
