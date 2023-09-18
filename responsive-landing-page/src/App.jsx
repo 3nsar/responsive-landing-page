9475453649b2febaf9a2ce99c8e919e8298f8245
3nsar <84131267+3nsar@users.noreply.github.com>
2023-09-18 17:23:45 +0200

    commit

--- responsive-landing-page/src/App.jsx
+++ responsive-landing-page/src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import CardSlider from './components/CardSlider'
 import About from './components/About'
+import Gallery from './components/Gallery'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero/>
       <CardSlider/>
       <About/>
+      <Gallery/>
     </div>
   )
 }
