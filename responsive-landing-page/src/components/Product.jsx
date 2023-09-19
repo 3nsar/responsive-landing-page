8c0f7b418a0224fbb3b1c596c6dda16d2cbf613d
3nsar <84131267+3nsar@users.noreply.github.com>
2023-09-19 17:40:11 +0200

    added loading

--- responsive-landing-page/src/components/Product.jsx
+++ responsive-landing-page/src/components/Product.jsx
@@ -1,26 +1,46 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import { useParams, Link } from 'react-router-dom'
+
+
 const Product = () => {
 
   const {id} = useParams()
   const url = `https://dummyjson.com/products/${id}`
   const[product, setProduct] = useState([])
+  const [loading, setLoading] = useState(true)
 
   useEffect(()=>{
     const fetchData = async ()=>{
+      setLoading(true)
       const res = await axios.get(url)
       console.log(res.data)
       setProduct(res.data)
+      setLoading(false)
     }
+    
     fetchData()
-  },[])
+  },[id])
 
+
+  if(loading){
   return (
-    <div className='product-cotnainer'>
-      <h1>{product.title}</h1>
+
+      <div className='loading-container'>LOADING....</div>
+    )
+  }else{
+    return(
+      <div className='product-container'>
+      <div className="product-content">
+      <img src={product.thumbnail} alt="pic" />
+        <p>{product.brand}</p>
+        <p>{product.title}</p>
+        <p>{product.price}$</p>
+      </div>
+      <Link  to={`/product/${product.id + 1}`}><button>NEXT</button></Link>
     </div>
-  )
+    )
+  }
 }
 
 export default Product
